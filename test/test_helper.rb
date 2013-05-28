966125cfe081db9f4840e9e3835cd740d49a205d
Jamie Wright <jamie@brilliantfantastic.com>
2013-05-27 23:10:18 -0400

    Updated test_helper with integration test information

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,14 +3,14 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/pride"
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
+require "minitest/rails/capybara"
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  include Rails.application.routes.url_helpers
+  include Capybara::RSpecMatchers
+  include Capybara::DSL
 end
