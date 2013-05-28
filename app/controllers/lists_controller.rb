5e01406a2f1f9b093d5e234e7403c17e38d11ad3
Jamie Wright <jamie@brilliantfantastic.com>
2013-05-28 13:35:38 -0400

    Display the size of each list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,9 @@
 class ListsController < ApplicationController
   def index
-    @lists = [WishList.new(name: 'Music'),
-              WishList.new(name: 'Home')]
+    @lists = []
+    @lists << WishList.new('Music',
+                           [Item.new(name: 'Angels and Airwaves'),
+                            Item.new(name: 'The National')])
+    @lists <<  WishList.new('Home', [Item.new(name: 'New blinds')])
   end
 end
--- test/features/displaying_wishlists_test.rb
+++ test/features/displaying_wishlists_test.rb
@@ -4,6 +4,8 @@ feature 'Displaying Wishlists Feature Test' do
   scenario 'has wishlists' do
     visit lists_path
     page.must_have_content "Music"
+    page.must_have_content "(2)"
     page.must_have_content "Home"
+    page.must_have_content "(1)"
   end
 end
