39c6bf1205ce6891f228caf4d1b47e608ea74bb6
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-11 01:55:29 -0400

    Implemented scenario for displaying error on page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create params[:user]
+    render :new and return unless @user.valid?
     redirect_to lists_path
   end
 end
