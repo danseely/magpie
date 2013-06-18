e77776424a3219ac92d24c7d62439f534c795b69
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-18 00:27:46 -0400

    Created migration for adding user id foreign key on wishlists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611045106) do
+ActiveRecord::Schema.define(:version => 20130618042444) do
 
   create_table "users", :force => true do |t|
     t.string   "email",      :null => false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20130611045106) do
     t.integer  "item_count"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
 end
