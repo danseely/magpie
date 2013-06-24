51e5900a591d141e6ada9304f0100902686bb871
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-23 22:13:56 -0400

    Handled invalid signin in the controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = login params[:user][:email], params[:user][:password]
+    unless @user
+      @user = User.new
+      render :new, error: 'Invalid email or password' and return
+    end
     redirect_to lists_path, notice: "Welcome #{@user.name}"
   end
 end
