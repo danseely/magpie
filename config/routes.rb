9a599dc9f4501ad9a36f31d754746187c2eedb54
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-23 21:19:56 -0400

    Implemented view and added controller route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Magpie::Application.routes.draw do
   resources :users, only: [:show, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signin' => 'sessions#new'
+  match '/signout' => 'sessions#destroy'
+
   match '/lists' => 'lists#index'
   match '/lists/:id' => 'lists#show'
 end
--- test/features/signin_test.rb
+++ test/features/signin_test.rb
@@ -7,5 +7,8 @@ feature 'Signin Feature Test' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Sign In'
+
+    current_path.must_equal lists_path
+    page.must_have_content "Welcome #{user.first_name} #{user.last_name}"
   end
 end
