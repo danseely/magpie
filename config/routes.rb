a8a12e655543f4424b94c001432e247c9958bed2
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-23 21:34:43 -0400

    Renamed users#new controller method to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Magpie::Application.routes.draw do
   resources :users, only: [:show, :new, :create]
+  match '/signup' => 'users#new'
+
   resources :sessions, only: [:new, :create, :destroy]
   match '/signin' => 'sessions#new'
   match '/signout' => 'sessions#destroy'
 
   match '/lists' => 'lists#index'
   match '/lists/:id' => 'lists#show'
+
+  root to: redirect('/signup')
 end
