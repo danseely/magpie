f37d4ff32e842fede33e7dcd786f4cc3d41c7539
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-23 20:39:04 -0400

    Added routes, controller, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Magpie::Application.routes.draw do
   resources :users, only: [:show, :new, :create]
+  match '/signin' => 'sessions#new'
   match '/lists' => 'lists#index'
   match '/lists/:id' => 'lists#show'
 end
