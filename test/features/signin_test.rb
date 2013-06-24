64c608b464fb5fce4d0f2c1670ade309ff8bfee2
Jamie Wright <jamie@brilliantfantastic.com>
2013-06-23 22:07:30 -0400

    Added failing scenario for invalid email address and password

--- test/features/signin_test.rb
+++ test/features/signin_test.rb
@@ -11,4 +11,15 @@ feature 'Signin Feature Test' do
     current_path.must_equal lists_path
     page.must_have_content "Welcome #{user.name}"
   end
+
+  scenario 'signining in with an email address and password that do not match' do
+    user = FactoryGirl.create :user, password: 'testing123'
+    visit signin_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'p@ssword'
+    click_button 'Sign In'
+
+    current_path.must_equal signin_path
+    page.must_have_content "Invalid email or password"
+  end
 end
