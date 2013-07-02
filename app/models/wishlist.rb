4805a28a9f0f0e074e0c8507c157dc0626718f80
Jamie Wright <jamie@brilliantfantastic.com>
2013-07-02 00:06:33 -0400

    Added name required validation for wishlist

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,4 +2,6 @@ class Wishlist < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :item_count, :name, :user_id
+
+  validates :name, :presence => true
 end
--- test/models/wishlist_test.rb
+++ test/models/wishlist_test.rb
@@ -10,4 +10,14 @@ describe Wishlist do
       wishlist.user_id.must_equal user.id
     end
   end
+
+  describe 'validations' do
+    let(:wishlist) { FactoryGirl.build :wishlist }
+
+    it 'must have a name' do
+      wishlist.name = ''
+      wishlist.valid?.wont_equal true
+      wishlist.errors[:name].must_include "can't be blank"
+    end
+  end
 end
